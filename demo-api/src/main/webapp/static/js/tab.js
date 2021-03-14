a824738c6c5a60b7e4b4b59fdf8be6753de6c577
林志华 <linzhihua@pansky.com.cn>
2021-03-15 00:59:34 +0800

    删除对应的jsp相关信息

--- demo-api/src/main/webapp/static/js/tab.js
+++
@@ -1,1 +0,0 @@
-function setTab(b,c,e){for(i=1;i<=e;i++){var d=document.getElementById(b+i);var a=document.getElementById("con_"+b+"_"+i);d.className=i==c?"hover":"";a.style.display=i==c?"block":"none"}};
